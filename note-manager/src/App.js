b341a52c9b232edcfb96ed804a3c2e6e9487a766
lightlingqqw <1390323642@qq.com>
2024-03-25 12:20:18 +0800

    路由测试

--- note-manager/src/App.js
+++ note-manager/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import A from "./pages/A/index.tsx";
+import B from "./pages/B/index.tsx";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/a" element={<A/>} />
+        <Route path="/b" element={<B/>} />
+      </Routes>
     </div>
   );
 }
